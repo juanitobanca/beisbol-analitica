c481591e5b0af46853dc1d35ac9e342e350680e9
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-25 21:15:37 -0700

    Pitches HEatMaps

--- data/base/procedimientos/pitches.sql
+++ data/base/procedimientos/pitches.sql
@@ -67,29 +67,7 @@ SELECT
   hardness,
   location,
   coordX,
-  coordY,
-  CASE
-    WHEN y <= 43 THEN 'FHP'
-    WHEN x >= 125 AND y >= 2.41 * x - 258.25 THEN 'RF1'
-    WHEN x >= 125 AND y >= x-82 THEN 'RF2'
-    WHEN x >= 125 THEN 'FRF'
-    WHEN x <= 125 AND y >= -2.41 * x + 344.25 THEN 'LF1'
-    WHEN x <= 125 AND y >= -x + 169  THEN 'LF2'
-    WHEN x <= 125 THEN 'FLF'
-  END heatMapFour,
-  CASE
-    WHEN y <= 43 THEN 'FHP'
-    WHEN x >= 125 AND y >= 5.03 * x - 585.75 THEN 'RF1'
-    WHEN x >= 125 AND y >= 2.41 * x - 258.25 THEN 'RF2'
-    WHEN x >= 125 AND y >= 1.5 * x - 144.5 THEN 'RF3'
-    WHEN x >= 125 AND y >= x-82 THEN 'RF4'
-    WHEN x >= 125 THEN 'FRF'
-    WHEN x <= 125 AND y >= -5.03 * x + 671.75 THEN 'LF1'
-    WHEN x <= 125 AND y >= -2.41 * x + 344.25 THEN 'LF2'
-    WHEN x <= 125 AND y >= -1.5 * x + 230.5 THEN 'LF3'
-    WHEN x <= 125 AND y >= -x + 169  THEN 'LF4'
-    WHEN x <= 125 THEN 'FLF'
-  END heatMapEight
+  -1 * ( coordY - 250 ) coordY
 FROM stg_play_pitch
 WHERE
   1 = 1
@@ -152,6 +130,30 @@ LEFT JOIN (
   ,    p.startStrikes = Coalesce( q.endStrikes, 0 )
   Where 1 = 1
   And   ( p.startBalls Is Null Or p.startStrikes Is Null );
+
+UPDATE pitches
+SET heatMapFour = CASE
+    WHEN CoordY <= 43 THEN 'FHP'
+    WHEN CoordX >= 125 AND CoordY >= 2.41 * CoordX - 258.25 THEN 'RF1'
+    WHEN CoordX >= 125 AND CoordY >= CoordX-82 THEN 'RF2'
+    WHEN CoordX >= 125 THEN 'FRF'
+    WHEN CoordX <= 125 AND CoordY >= -2.41 * CoordX + 344.25 THEN 'LF1'
+    WHEN CoordX <= 125 AND CoordY >= -CoordX + 169  THEN 'LF2'
+    WHEN CoordX <= 125 THEN 'FLF'
+  END,
+heatMapEight = CASE
+    WHEN CoordY <= 43 THEN 'FHP'
+    WHEN CoordX >= 125 AND CoordY >= 5.03 * CoordX - 585.75 THEN 'RF1'
+    WHEN CoordX >= 125 AND CoordY >= 2.41 * CoordX - 258.25 THEN 'RF2'
+    WHEN CoordX >= 125 AND CoordY >= 1.5 * CoordX - 144.5 THEN 'RF3'
+    WHEN CoordX >= 125 AND CoordY >= CoordX-82 THEN 'RF4'
+    WHEN CoordX >= 125 THEN 'FRF'
+    WHEN CoordX <= 125 AND CoordY >= -5.03 * CoordX + 671.75 THEN 'LF1'
+    WHEN CoordX <= 125 AND CoordY >= -2.41 * CoordX + 344.25 THEN 'LF2'
+    WHEN CoordX <= 125 AND CoordY >= -1.5 * CoordX + 230.5 THEN 'LF3'
+    WHEN CoordX <= 125 AND CoordY >= -CoordX + 169  THEN 'LF4'
+    WHEN CoordX <= 125 THEN 'FLF'
+  END;
 
 COMMIT;
 
