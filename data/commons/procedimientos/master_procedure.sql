13836d05f2b7eeea48b65f3f8a4e9501da6ffc9d
Juanito Banca <jose_alvarado89@hotmail.com>
2021-10-04 21:54:26 -0700

    Attributes in REM tables.

--- data/commons/procedimientos/master_procedure.sql
+++ data/commons/procedimientos/master_procedure.sql
@@ -210,6 +210,7 @@ CALL update_table_attributes('pf_park_factors', @update_stmt);
 CALL update_table_attributes('pf_heat_map_park_factors', @update_stmt);
 CALL update_table_attributes('agg_team_performance_stats', @update_stmt);
 CALL update_table_attributes('rem_event_run_value', @update_stmt);
+CALL update_table_attributes('rem_run_expectancy_matrix', @update_stmt);
 
 
 -- Clean Staging Tables
