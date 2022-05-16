d22bce52019308fc8dd2bf55e7e12f9a07d04ce6
Juanito Banca <jose_alvarado89@hotmail.com>
2022-05-15 18:58:48 -0700

    Update agg_fielding_stats.sql

--- sqlite3/tablas/agg_fielding_stats.sql
+++ sqlite3/tablas/agg_fielding_stats.sql
@@ -9,10 +9,10 @@ seasonId REAL,
 gameDate TEXT,
 gameType2 TEXT,
 teamType TEXT,
-venueId REAL,
-teamId REAL,
+venueId INTEGER,
+teamId INTEGER,
 positionAbbrev TEXT,
-playerId REAL,
+playerId INTEGER,
 assists INTEGER,
 catcherInterferences INTEGER,
 errors INTEGER,
@@ -31,3 +31,9 @@ teamName TEXT,
 venueName TEXT
 );
 
+CREATE INDEX groupingId_agg_fielding_stats ON agg_fielding_stats(groupingId);
+CREATE INDEX majorLeagueId_agg_fielding_stats ON agg_fielding_stats(majorLeagueId);
+CREATE INDEX playerId_agg_fielding_stats ON agg_fielding_stats(playerId);
+CREATE INDEX seasonId_agg_fielding_stats ON agg_fielding_stats(seasonId);
+CREATE INDEX teamId_agg_fielding_stats ON agg_fielding_stats(teamId);
+CREATE INDEX venueId_agg_fielding_stats ON agg_fielding_stats(venueId);
