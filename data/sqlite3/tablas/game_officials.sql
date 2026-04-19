6eb168ef8fc75b90a52c4df269fa6eae4c016d72
martilllo <15705246+juanitobanca@users.noreply.github.com>
2026-04-18 21:07:55 -0700

    Delete all this files

--- data/sqlite3/tablas/game_officials.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS game_officials; 
-
-CREATE TABLE game_officials (
-gamePk INTEGER,
-officialId INTEGER,
-position TEXT
-);
-
-CREATE INDEX gamePk_game_officials ON game_officials(gamePk);
--- data/sqlite3/tablas/major_leagues.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS major_leagues; 
-
-CREATE TABLE major_leagues (
-majorLeagueId INTEGER,
-majorLeague TEXT
-);
-
-CREATE INDEX majorLeagueId_major_leagues ON major_leagues(majorLeagueId);
