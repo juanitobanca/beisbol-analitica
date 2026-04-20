fafdb95f423a7894b78974239550ad0762b099d7
martilllo <15705246+juanitobanca@users.noreply.github.com>
2026-04-19 20:47:07 -0700

    Update setup.sql

--- data/setup/setup.sql
+++ data/setup/setup.sql
@@ -1,4 +1,5 @@
 -- Tables
+-- Stg
 .read ./data/staging/tablas/stg_box_player_pitching.sql
 .read ./data/staging/tablas/stg_box_player_game_positions.sql
 .read ./data/staging/tablas/stg_transactions.sql
@@ -21,19 +22,8 @@
 .read ./data/staging/tablas/stg_box_info.sql
 .read ./data/staging/tablas/stg_box_team_pitching.sql
 .read ./data/staging/tablas/stg_box_team_batting.sql
-.read ./data/park_factors/tablas/pf_heat_map_park_factors.sql
-.read ./data/park_factors/tablas/pf_park_factors.sql
-.read ./data/run_expectancy/tablas/rem_event_run_value.sql
-.read ./data/run_expectancy/tablas/rem_play_by_play.sql
-.read ./data/run_expectancy/tablas/rem_run_expectancy_matrix.sql
-.read ./data/agregados/pitching/tablas/agg_pitching_stats.sql
-.read ./data/agregados/pitching/tablas/agg_pitching_balls_in_play_heatmaps.sql
-.read ./data/agregados/batting/tablas/agg_batting_balls_in_play_heatmaps.sql
-.read ./data/agregados/batting/tablas/agg_batting_stats.sql
-.read ./data/agregados/team_performance/tablas/agg_team_performance_stats.sql
-.read ./data/agregados/fielding/tablas/agg_fielding_stats.sql
-.read ./data/win_expectancy/tablas/we_win_probability_added.sql
-.read ./data/win_expectancy/tablas/we_win_expectancy.sql
+
+-- Base
 .read ./data/base/tablas/runners.sql
 .read ./data/base/tablas/game_officials.sql
 .read ./data/base/tablas/officials.sql
@@ -57,39 +47,32 @@
 .read ./data/base/tablas/game_player_positions.sql
 .read ./data/base/tablas/major_leagues.sql
 
--- Funcs
-.read ./data/commons/funciones/agg_grouping_description.sql
-.read ./data/commons/funciones/agg_or_cum_queries.sql
-.read ./data/commons/funciones/agg_grouping_id.sql
+-- Park Factors
+.read ./data/park_factors/tablas/pf_heat_map_park_factors.sql
+.read ./data/park_factors/tablas/pf_park_factors.sql
+
+-- Run Expectancy
+.read ./data/run_expectancy/tablas/rem_event_run_value.sql
+.read ./data/run_expectancy/tablas/rem_play_by_play.sql
+.read ./data/run_expectancy/tablas/rem_run_expectancy_matrix.sql
+
+-- Win Expectancy
+.read ./data/win_expectancy/tablas/we_win_probability_added.sql
+.read ./data/win_expectancy/tablas/we_win_expectancy.sql
+
+-- Aggs
+.read ./data/agregados/pitching/tablas/agg_pitching_stats.sql
+.read ./data/agregados/pitching/tablas/agg_pitching_balls_in_play_heatmaps.sql
+.read ./data/agregados/batting/tablas/agg_batting_balls_in_play_heatmaps.sql
+.read ./data/agregados/batting/tablas/agg_batting_stats.sql
+.read ./data/agregados/team_performance/tablas/agg_team_performance_stats.sql
+.read ./data/agregados/fielding/tablas/agg_fielding_stats.sql
 
 -- Procs
+-- Stg to Base
 .read ./data/staging/procedimientos/clean_staging_tables.sql
-.read ./data/park_factors/procedimientos/pf_heat_map_park_factors.sql
-.read ./data/park_factors/procedimientos/pf_park_factors.sql
-.read ./data/commons/procedimientos/update_table_attributes.sql
-.read ./data/commons/procedimientos/master_procedure.sql
-.read ./data/run_expectancy/procedimientos/rem_event_run_value.sql
-.read ./data/run_expectancy/procedimientos/rem_play_by_play.sql
-.read ./data/run_expectancy/procedimientos/rem_run_expectancy_matrix.sql
-.read ./data/agregados/pitching/procedimientos/agg_pitching_stats.sql
-.read ./data/agregados/pitching/procedimientos/agg_pitching_derived_metrics.sql
-.read ./data/agregados/pitching/procedimientos/fip.sql
-.read ./data/agregados/pitching/procedimientos/agg_pitching_split_stats.sql
-.read ./data/agregados/pitching/procedimientos/agg_pitching_balls_in_play_heatmaps.sql
-.read ./data/agregados/batting/procedimientos/wraa.sql
-.read ./data/agregados/batting/procedimientos/agg_batting_split_stats.sql
-.read ./data/agregados/batting/procedimientos/agg_batting_balls_in_play_heatmaps.sql
-.read ./data/agregados/batting/procedimientos/agg_batting_derived_metrics.sql
-.read ./data/agregados/batting/procedimientos/agg_batting_stats.sql
-.read ./data/agregados/batting/procedimientos/ops_plus.sql
-.read ./data/agregados/batting/procedimientos/woba.sql
-.read ./data/agregados/batting/procedimientos/wrc.sql
-.read ./data/agregados/team_performance/procedimientos/agg_team_performance_derived_metrics.sql
-.read ./data/agregados/team_performance/procedimientos/agg_team_performance_stats.sql
-.read ./data/agregados/fielding/procedimientos/agg_fielding_stats.sql
-.read ./data/agregados/fielding/procedimientos/agg_fielding_derived_metrics.sql
-.read ./data/win_expectancy/procedimientos/we_win_probability_added.sql
-.read ./data/win_expectancy/procedimientos/we_win_expectancy.sql
+
+-- Base
 .read ./data/base/procedimientos/runners.sql
 .read ./data/base/procedimientos/game_officials.sql
 .read ./data/base/procedimientos/officials.sql
@@ -112,3 +95,36 @@
 .read ./data/base/procedimientos/defensive_substitutions.sql
 .read ./data/base/procedimientos/game_player_positions.sql
 .read ./data/base/procedimientos/major_leagues.sql
+
+-- Park Factors
+.read ./data/park_factors/procedimientos/pf_heat_map_park_factors.sql
+.read ./data/park_factors/procedimientos/pf_park_factors.sql
+
+-- Run Expectancy
+.read ./data/run_expectancy/procedimientos/rem_event_run_value.sql
+.read ./data/run_expectancy/procedimientos/rem_play_by_play.sql
+.read ./data/run_expectancy/procedimientos/rem_run_expectancy_matrix.sql
+
+-- Win Expectancy
+.read ./data/win_expectancy/procedimientos/we_win_probability_added.sql
+.read ./data/win_expectancy/procedimientos/we_win_expectancy.sql
+
+-- Aggs
+.read ./data/agregados/pitching/procedimientos/agg_pitching_stats.sql
+.read ./data/agregados/pitching/procedimientos/agg_pitching_derived_metrics.sql
+.read ./data/agregados/pitching/procedimientos/fip.sql
+.read ./data/agregados/pitching/procedimientos/agg_pitching_split_stats.sql
+.read ./data/agregados/pitching/procedimientos/agg_pitching_balls_in_play_heatmaps.sql
+.read ./data/agregados/batting/procedimientos/wraa.sql
+.read ./data/agregados/batting/procedimientos/agg_batting_stats.sql
+.read ./data/agregados/batting/procedimientos/agg_batting_split_stats.sql
+.read ./data/agregados/batting/procedimientos/agg_batting_balls_in_play_heatmaps.sql
+.read ./data/agregados/batting/procedimientos/agg_batting_derived_metrics.sql
+.read ./data/agregados/batting/procedimientos/ops_plus.sql
+.read ./data/agregados/batting/procedimientos/woba.sql
+.read ./data/agregados/batting/procedimientos/wrc.sql
+
+.read ./data/agregados/team_performance/procedimientos/agg_team_performance_derived_metrics.sql
+.read ./data/agregados/team_performance/procedimientos/agg_team_performance_stats.sql
+.read ./data/agregados/fielding/procedimientos/agg_fielding_stats.sql
+.read ./data/agregados/fielding/procedimientos/agg_fielding_derived_metrics.sql
